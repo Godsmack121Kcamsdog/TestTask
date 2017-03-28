81896d0c9bd6c7d2ada41060d10c281a17196b34
Kucherenko Alex <andreykoalex@gmail.com>
2017-03-28 22:12:02 +0300

    pictures added

--- app/src/main/java/com/testapplication/project/kucherenko/dnu/testapplication/adapters/MatchAdapter.java
+++ app/src/main/java/com/testapplication/project/kucherenko/dnu/testapplication/adapters/MatchAdapter.java
@@ -1,6 +1,7 @@
 package com.testapplication.project.kucherenko.dnu.testapplication.adapters;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -54,6 +55,7 @@ public class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.MatchHolder>
 
         holder.textCount.setText(holder.team1
                 .concat("-").concat(count1 + " ").concat(":").concat(" " + count2).concat("-").concat(holder.team2));
+        holder.setTeamImages(holder.team1, holder.team2);
     }
 
     @Override
@@ -74,11 +76,89 @@ public class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.MatchHolder>
             homeCountryImage = (ImageView) itemView.findViewById(R.id.imageView);
             anotherCountryImage = (ImageView) itemView.findViewById(R.id.imageView2);
             textCount = (TextView) itemView.findViewById(R.id.textView);
-            setTeamImages(team1, team2);
         }
 
-        private void setTeamImages(String team1, String team2){
+        private void setTeamImages(String team1, String team2) {
+            int id1;
+            int id2;
 
+            switch (team1) {
+                case "Switzerland": {
+                    id1 = R.drawable.switzerland;
+                    break;
+                }
+                case "Wales": {
+                    id1 = R.drawable.wales;
+                    break;
+                }
+                case "Croatia": {
+                    id1 = R.drawable.croatia;
+                    break;
+                }
+                case "France": {
+                    id1 = R.drawable.france;
+                    break;
+                }
+                case "Germany": {
+                    id1 = R.drawable.germany;
+                    break;
+                }
+                case "Hungary": {
+                    id1 = R.drawable.hungary;
+                    break;
+                }
+                case "Italy": {
+                    id1 = R.drawable.italy;
+                    break;
+                }
+                case "England": {
+                    id1 = R.drawable.england;
+                    break;
+                }
+                default: {
+                    id1 = R.mipmap.ic_launcher;
+                }
+            }
+
+            switch (team2) {
+                case "Poland": {
+                    id2 = R.drawable.poland;
+                    break;
+                }
+                case "NorthernIreland": {
+                    id2 = R.drawable.northern_irland;
+                    break;
+                }
+                case "Portugal": {
+                    id2 = R.drawable.portugal;
+                    break;
+                }
+                case "Republic of Ireland": {
+                    id2 = R.drawable.irland;
+                    break;
+                }
+                case "Slovakia": {
+                    id2 = R.drawable.slovakia;
+                    break;
+                }
+                case "Belgium": {
+                    id2 = R.drawable.belgium;
+                    break;
+                }
+                case "Spain": {
+                    id2 = R.drawable.spain;
+                    break;
+                }
+                case "Iceland": {
+                    id2 = R.drawable.iceland;
+                    break;
+                }
+                default: {
+                    id2 = R.mipmap.ic_launcher;
+                }
+            }
+            homeCountryImage.setBackground((context).getDrawable(id1));
+            anotherCountryImage.setBackground((context).getDrawable(id2));
         }
     }
 }
